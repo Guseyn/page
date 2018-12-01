ad2563170171d3f4c7fa10cd8485791cd5dacbb9
gismayilov <guseynism@gmail.com>
2018-12-02 00:57:17 +0300

    some example of using unit

--- static/js/es6/Page.js
+++ static/js/es6/Page.js
@@ -1,9 +1,27 @@
 const { Unit } = require('@page-libs/unit');
 
+const { ElementWithAppendedChildren, ElementWithInnerHTML, div } = require('@page-libs/dom'); 
+const { ResponseFromAjaxRequest, ResponseBody } = require('@page-libs/ajax');
+
 class Page extends Unit {
 
-  constructor(elm) {
+  constructor(elm, button) {
     super(elm);
+    button.override('onclick', this.onButtonClick);
+  }
+
+  onButtonClick() {
+    new ElementWithAppendedChildren(this.elm,
+      new ElementWithInnerHTML(
+        div('class="res"')(),
+        new ResponseBody(
+          new ResponseFromAjaxRequest({
+            url: 'http://localhost:8000/html/res.html',
+            method: 'GET'
+          })
+        )
+      )
+    ).call();
   }
 
 }
